277fa8c93ba26e1cdc4ad2e15064e12a90dc40d0
Gaurav Narkhede <narkhedegs@gmail.com>
2015-09-22 15:34:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Goonj
+Goonj is simple C# command line utility that can be used for testing Process API in .NET.
+
+# Installation
+Goonj is available at [Nuget](https://www.nuget.org/packages/Goonj/) and can be installed as a package using VisualStudio NuGet package manager or via the NuGet command line:
+> Install-Package Goonj
+
+# Quick Start
+
+# Advance Usage
+
+# Phantom Options
+
+# To Do
---
+++ Source/Goonj/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Narkhedegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                ShowUsage();
+            }
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Goonj - echoes command-line arguments to stdout/stderr.");
+            Console.WriteLine("");
+            Console.WriteLine("Usage: goonj [options]");
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine("  --help             Displays how the tool is supposed to be used.");
+        }
+    }
+}
